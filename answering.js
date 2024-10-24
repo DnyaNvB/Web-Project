b77c51f7828f3cc1866ea4d3d589cc8f40cbe4a8
DnyaNvB <dnya.nvb@gmail.com>
2024-10-25 00:13:50 +0330

    add primary files

---
+++ answering.js
@@ -0,0 +1,47 @@
+const btns = document.querySelectorAll(".acc-btn");
+
+// fn
+function filterQuestions(difficulty) {
+  const items = document.querySelectorAll('.acc-item');
+
+  items.forEach(item => {
+    if (difficulty === 'all' || item.getAttribute('data-difficulty') === difficulty) {
+      item.style.display = 'block'; // Show item
+    } else {
+      item.style.display = 'none'; // Hide item
+    }
+  });
+}
+
+function accordion() {
+  // this = the btn | icon & bg changed
+  this.classList.toggle("is-open");
+
+  // the acc-content
+  const content = this.nextElementSibling;
+
+  // IF open, close | else open
+  if (content.style.maxHeight) content.style.maxHeight = null;
+  else content.style.maxHeight = content.scrollHeight + "px";
+}
+
+// event
+btns.forEach((el) => el.addEventListener("click", accordion));
+
+/*
+
+       Jokes are from >
+        https://chartcons.com/100-funny-trick-questions-answers/
+        Background image from >
+        https://www.magicpattern.design/tools/css-backgrounds
+
+*/
+document.getElementById('mode-toggle').addEventListener('click', function() {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    this.textContent = 'Switch to Light Mode';
+  } else {
+    this.textContent = 'Switch to Dark Mode';
+  }
+});
---
+++ signup_login.js
@@ -0,0 +1,9 @@
+document.getElementById('mode-toggle').addEventListener('click', function() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+      this.textContent = 'Switch to Light Mode';
+    } else {
+      this.textContent = 'Switch to Dark Mode';
+    }
+  });
