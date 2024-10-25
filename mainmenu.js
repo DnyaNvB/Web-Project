b2927c451414d4f5108d3792d0374e09e2f387fe
Donya Navabi <nvbdnya@gmail.com>
2024-10-25 16:42:02 +0330

    Make Dark mode for some pages

--- mainmenu.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('mode-toggle').addEventListener('click', function() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-    if (body.classList.contains('dark-mode')) {
-      this.textContent = 'Switch to Light Mode';
-    } else {
-      this.textContent = 'Switch to Dark Mode';
-    }
-  });
