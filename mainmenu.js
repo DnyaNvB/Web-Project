89b4f72814162a243096078662ff6c714d2edf98
Donya Navabi <arya.torabi.deadarya@gmail.com>
2024-10-25 18:40:54 +0330

    6.40

--- mainmenu.js
+++ mainmenu.js
@@ -1,9 +1,13 @@
-document.getElementById('mode-toggle').addEventListener('click', function() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-    if (body.classList.contains('dark-mode')) {
-      this.textContent = 'Switch to Light Mode';
-    } else {
-      this.textContent = 'Switch to Dark Mode';
-    }
-  });
+document.getElementById('mode-toggle').addEventListener('click', function () {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    this.textContent = 'Switch to Light Mode';
+  } else {
+    this.textContent = 'Switch to Dark Mode';
+  }
+});
+document.getElementById('logout-button').addEventListener('click', function () {
+  // Add your logout logic here, such as redirecting to a logout page
+  window.location.href = 'signinlogin.html'; // Example redirect to a logout page
+});
--- questions.js
+++ questions.js
@@ -46,3 +46,8 @@ document.getElementById('mode-toggle').addEventListener('click', function() {
     this.textContent = 'Switch to Dark Mode';
   }
 });
+
+document.getElementById('logout-button').addEventListener('click', function() {
+  // Add your logout logic here, such as redirecting to a logout page
+  window.location.href = 'mainmenu.html'; // Example redirect to a logout page
+});
